0399cb393b7d92ca2901acf5794af69879fec224
Vansh Malhotra <85389767+Vansh3503@users.noreply.github.com>
2024-05-30 16:21:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,14 @@ import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
-
+import streamlit as st
 def load_environment():
     load_dotenv()
-    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-    os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
+  
     os.environ["LANGCHAIN_TRACING_V2"] = "true"  # Langsmith tracking
 
 # Initialize the language model
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="Llama3-8b-8192")
+llm = ChatGroq(groq_api_key=st.secrets["GROQ_API_KEY"], model_name="Llama3-8b-8192")
 
 # Define the prompt template
 prompt = ChatPromptTemplate.from_template(
