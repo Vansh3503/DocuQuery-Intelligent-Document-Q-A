3695eebefd9548de4cedb4dcd363c605debef4c8
Vansh Malhotra <85389767+Vansh3503@users.noreply.github.com>
2024-05-30 16:22:21 +0530

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -9,7 +9,7 @@ def vector_embedding_for_pdf(pdf_file):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
         tmp_file.write(pdf_file.getvalue())
         tmp_file_path = tmp_file.name
-    st.session_state.embedding = OpenAIEmbeddings()
+    st.session_state.embedding = OpenAIEmbeddings(openai_api_key=st.secrets["OPENAI_API_KEY"])
     st.session_state.loader = PyPDFLoader(tmp_file_path)
     st.session_state.docs = st.session_state.loader.load()
     st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=300)
@@ -20,7 +20,7 @@ def vector_embedding_for_text(text_file):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".txt") as tmp_file:
         tmp_file.write(text_file.getvalue())
         tmp_file_path = tmp_file.name
-    st.session_state.embedding = OpenAIEmbeddings()
+    st.session_state.embedding = OpenAIEmbeddings(openai_api_key=st.secrets["OPENAI_API_KEY"])
     st.session_state.loader = TextLoader(tmp_file_path)
     st.session_state.docs = st.session_state.loader.load()
     st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=300)
@@ -28,7 +28,7 @@ def vector_embedding_for_text(text_file):
     st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embedding)
 
 def vector_embedding_for_web(url):
-    st.session_state.embedding = OpenAIEmbeddings()
+    st.session_state.embedding = OpenAIEmbeddings(openai_api_key=st.secrets["OPENAI_API_KEY"])
     st.session_state.loader = WebBaseLoader(url)
     st.session_state.docs = st.session_state.loader.load()
     st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=300)
